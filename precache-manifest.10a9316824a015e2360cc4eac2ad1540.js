d1d199e4825b968519635107e251d3bc545473ec
ATOM03 <priyanshujhaginia@outlook.com>
2020-05-05 18:46:20 +0530

    Updates

---
+++ precache-manifest.10a9316824a015e2360cc4eac2ad1540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a275665f077a75a0f0ee09636d37ac",
+    "url": "/Chat-App1/index.html"
+  },
+  {
+    "revision": "a2387a21b2d1299a7aa4",
+    "url": "/Chat-App1/static/css/main.43a11098.chunk.css"
+  },
+  {
+    "revision": "acc8dcc58c67885f24ce",
+    "url": "/Chat-App1/static/js/2.bc867ec9.chunk.js"
+  },
+  {
+    "revision": "1d39e838bf8e50206fcdcefbfaa44e19",
+    "url": "/Chat-App1/static/js/2.bc867ec9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2387a21b2d1299a7aa4",
+    "url": "/Chat-App1/static/js/main.44d77261.chunk.js"
+  },
+  {
+    "revision": "40aae8ec8e868a1a2629",
+    "url": "/Chat-App1/static/js/runtime-main.100e5006.js"
+  }
+]);
